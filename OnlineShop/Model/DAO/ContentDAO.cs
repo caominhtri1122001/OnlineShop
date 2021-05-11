0e7c0db165ee47713400697cedbcaf1d8c477438
Cao Minh Tri <caominhtri112201@gmail.com>
2021-05-11 21:01:40 +0700

    Update CategoryDAO , ContentDAO , Api POST GET Category

---
+++ OnlineShop/Model/DAO/ContentDAO.cs
@@ -0,0 +1,23 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAO
+{
+    public class ContentDAO
+    {
+        OnlineShopDbContext db = null;
+        public ContentDAO()
+        {
+            db = new OnlineShopDbContext();
+        }
+
+        public Content GetByID(long ID)
+        {
+            return db.Contents.Find(ID);
+        }
+    }
+}
--- OnlineShop/OnlineShop/Areas/Admin/Controllers/ContentController.cs
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/ContentController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model.DAO;
+using Model.EF;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
@@ -14,9 +16,49 @@ namespace OnlineShop.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Create()
         {
+            SetViewBag();
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(long ID)
+        {
+            var dao = new ContentDAO();
+            var content = dao.GetByID(ID);
+            
+            SetViewBag(content.CategoryID);
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Content model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            SetViewBag(model.CategoryID);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Content model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            SetViewBag();
+            return View();
+        }
+        public void SetViewBag(long? selectedID = null)
+        {
+            var dao = new CategoryDAO();
+            ViewBag.CategoryID = new SelectList(dao.ListAll(),"ID","Name",selectedID);
         }
     }
 }
