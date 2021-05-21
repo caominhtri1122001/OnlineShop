2459701c5b4ebc6dc73a322a81731df6785ca4cc
Cao Minh Tri <caominhtri112201@gmail.com>
2021-05-21 21:01:59 +0700

    Update Register

--- OnlineShop/Model/DAO/UserDAO.cs
+++ OnlineShop/Model/DAO/UserDAO.cs
@@ -112,5 +112,15 @@ namespace Model.DAO
                 }
             }
         }
+
+        public bool CheckUserName (string userName)
+        {
+            return db.Users.Count(x => x.UserName == userName) > 0;
+        }
+
+        public bool CheckEmail(string email)
+        {
+            return db.Users.Count(x => x.Email == email) > 0;
+        }
     }
 }
--- OnlineShop/OnlineShop/App_Start/RouteConfig.cs
+++ OnlineShop/OnlineShop/App_Start/RouteConfig.cs
@@ -35,10 +35,23 @@ namespace OnlineShop
           );
 
             routes.MapRoute(
+              name: "Contact",
+              url: "lien-he",
+              defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional },
+              namespaces: new[] { "OnlineShop.Controllers" }
+          );
+
+            routes.MapRoute(
             name: "Cart",
             url: "gio-hang",
             defaults: new { controller = "Cart", action = "Index", id = UrlParameter.Optional },
             namespaces: new[] { "OnlineShop.Controllers" }
+        );
+            routes.MapRoute(
+           name: "Register",
+           url: "dang-ky",
+           defaults: new { controller = "User", action = "Register", id = UrlParameter.Optional },
+           namespaces: new[] { "OnlineShop.Controllers" }
         );
 
             routes.MapRoute(
